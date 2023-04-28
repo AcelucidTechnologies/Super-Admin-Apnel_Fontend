1ccf453e668dfc42c155e926911ca35fd5ae8803
Akhil Chauhan <achauhan@acelucid.com>
2023-04-28 19:56:51 +0530

    g

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   // BASE_URL:"http://localhost:5000",
   JSON_SERVER:"http://13.126.212.31:5000",
   BASE_URL:"http://13.126.212.31:5000",
-  BASE_URL2:"http://65.0.110.147:4000",
+  // BASE_URL2:"http://65.0.110.147:4000",
   IP_ADDRESS:"http://localhost:4200",
 };
 
